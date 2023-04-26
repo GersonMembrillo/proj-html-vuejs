5c27d361366e43f3ecedfe399b8fac605e519fce
GersonMembrillo <gersonmembrillo@hotmail.com>
2023-04-26 22:30:40 +0200

    start yay

---
+++ src/data/store.js
@@ -0,0 +1,29 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+	// apiURL: 'https://api.themoviedb.org/3/',
+	// params: {
+	// 	api_key: '7586603b576c9a016771a0ca65601fff',
+	// 	query: '',
+	// },
+	// imagePath: 'https://image.tmdb.org/t/p/',
+	// fileSize: 'w342',
+	// movie: {
+	// 	items: [],
+	// 	endpoint: 'search/movie',
+	// 	error: '',
+	// 	loader: false,
+	// },
+	// tv: {
+	// 	items: [],
+	// 	endpoint: 'search/tv',
+	// 	error: '',
+	// 	loader: false,
+	// },
+	// popular: {
+	// 	items: [],
+	// 	endpoint: 'search/top_rated',
+	// 	error: '',
+	// 	loader: false,
+	// },
+});
